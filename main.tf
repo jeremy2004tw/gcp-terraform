ff61763af3e313ba2aee8a088b695d9d3ae27d16
Jeremy <jeremy2004tw@gmail.com>
2026-08-20 11:07:48 -0500

    add project in main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "jj-github-demo-bucket"
+  project                  = "terraform-demo-504200"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"

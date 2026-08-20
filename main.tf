38e04f80ab1ee600edfec6a301149f09f3470dac
Jeremy <jeremy2004tw@gmail.com>
2026-08-20 10:45:25 -0500

    did terraform fmt

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "jj-github-demo-bucket"
-  location      = "US"
-  force_destroy = true
+  name                     = "jj-github-demo-bucket"
+  location                 = "US"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
